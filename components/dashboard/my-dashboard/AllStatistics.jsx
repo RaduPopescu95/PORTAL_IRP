a9e3cfc7a7ac418ae1257d4ab86c5913ab95eab1
Adrian <radu.p1995@yahoo.com>
2024-06-10 21:02:13 +0300

    tasda\

--- components/dashboard/my-dashboard/AllStatistics.jsx
+++ components/dashboard/my-dashboard/AllStatistics.jsx
@@ -12,35 +12,237 @@ const AllStatistics = async () => {
     {
       id: 1,
       blockStyle: "",
-      icon: "flaticon-profit",
       timer: "121",
+      name: "Comunicate/Buletine totale",
     },
     {
       id: 2,
       blockStyle: "style2",
-      icon: "flaticon-high-five",
       timer: "24",
-      name: "Solcitari verbale",
+      name: "Comunicate/Buletine ultima luna",
     },
     {
       id: 3,
       blockStyle: "style3",
-      icon: "flaticon-chat",
       timer: "12",
-      name: "Incendii",
+      name: "Solicitari verbale ultima luna",
     },
     {
       id: 4,
       blockStyle: "style4",
-      icon: "flaticon-heart",
       timer: "18",
+      name: "Solicitari scrise ultima luna",
+    },
+  ];
+
+  const interventiiSapt = [
+    {
+      id: 1,
+      blockStyle: "",
+      timer: "121",
+      name: "Incendii",
+    },
+    {
+      id: 2,
+      blockStyle: "style2",
+      timer: "24",
       name: "Incendii vegetatie",
+    },
+    {
+      id: 6,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Suprafete (ha)",
+    },
+    {
+      id: 3,
+      blockStyle: "style3",
+      timer: "12",
+      name: "Accidente rutiere",
+    },
+    {
+      id: 4,
+      blockStyle: "style4",
+      timer: "18",
+      name: "SMURD",
+    },
+    {
+      id: 5,
+      blockStyle: "style4",
+      timer: "18",
+      name: "Alte interventii",
+    },
+  ];
+  const interventiiLuna = [
+    {
+      id: 1,
+      blockStyle: "",
+      timer: "121",
+      name: "Incendii",
+    },
+    {
+      id: 2,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Incendii vegetatie",
+    },
+    {
+      id: 6,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Suprafete (ha)",
+    },
+    {
+      id: 3,
+      blockStyle: "style3",
+      timer: "12",
+      name: "Accidente rutiere",
+    },
+    {
+      id: 4,
+      blockStyle: "style4",
+      timer: "18",
+      name: "SMURD",
+    },
+    {
+      id: 5,
+      blockStyle: "style4",
+      timer: "18",
+      name: "Alte interventii",
+    },
+  ];
+  const interventiiTreiLuni = [
+    {
+      id: 1,
+      blockStyle: "",
+      timer: "121",
+      name: "Incendii",
+    },
+    {
+      id: 2,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Incendii vegetatie",
+    },
+    {
+      id: 6,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Suprafete (ha)",
+    },
+    {
+      id: 3,
+      blockStyle: "style3",
+      timer: "12",
+      name: "Accidente rutiere",
+    },
+    {
+      id: 4,
+      blockStyle: "style4",
+      timer: "18",
+      name: "SMURD",
+    },
+    {
+      id: 5,
+      blockStyle: "style4",
+      timer: "18",
+      name: "Alte interventii",
+    },
+  ];
+  const interventiiAnual = [
+    {
+      id: 1,
+      blockStyle: "",
+      timer: "121",
+      name: "Incendii",
+    },
+    {
+      id: 2,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Incendii vegetatie",
+    },
+    {
+      id: 6,
+      blockStyle: "style2",
+      timer: "24",
+      name: "Suprafete (ha)",
+    },
+    {
+      id: 3,
+      blockStyle: "style3",
+      timer: "12",
+      name: "Accidente rutiere",
+    },
+    {
+      id: 4,
+      blockStyle: "style4",
+      timer: "18",
+      name: "SMURD",
+    },
+    {
+      id: 5,
+      blockStyle: "style4",
+      timer: "18",
+      name: "Alte interventii",
     },
   ];
 
   return (
     <>
       {allStatistics.map((item) => (
+        <div className="col-sm-6 col-md-6 col-lg-6 col-xl-3" key={item.id}>
+          <div className={`ff_one ${item.blockStyle}`}>
+            <div className="detais">
+              <div className="timer">{numarOferte}</div>
+              <p>{item.name}</p>
+            </div>
+            <div className="icon">
+              <span className={item.icon}></span>
+            </div>
+          </div>
+        </div>
+      ))}
+      {interventiiSapt.map((item) => (
+        <div className="col-sm-6 col-md-6 col-lg-6 col-xl-3" key={item.id}>
+          <div className={`ff_one ${item.blockStyle}`}>
+            <div className="detais">
+              <div className="timer">{numarOferte}</div>
+              <p>{item.name}</p>
+            </div>
+            <div className="icon">
+              <span className={item.icon}></span>
+            </div>
+          </div>
+        </div>
+      ))}
+      {interventiiLuna.map((item) => (
+        <div className="col-sm-6 col-md-6 col-lg-6 col-xl-3" key={item.id}>
+          <div className={`ff_one ${item.blockStyle}`}>
+            <div className="detais">
+              <div className="timer">{numarOferte}</div>
+              <p>{item.name}</p>
+            </div>
+            <div className="icon">
+              <span className={item.icon}></span>
+            </div>
+          </div>
+        </div>
+      ))}
+      {interventiiTreiLuni.map((item) => (
+        <div className="col-sm-6 col-md-6 col-lg-6 col-xl-3" key={item.id}>
+          <div className={`ff_one ${item.blockStyle}`}>
+            <div className="detais">
+              <div className="timer">{numarOferte}</div>
+              <p>{item.name}</p>
+            </div>
+            <div className="icon">
+              <span className={item.icon}></span>
+            </div>
+          </div>
+        </div>
+      ))}
+      {interventiiAnual.map((item) => (
         <div className="col-sm-6 col-md-6 col-lg-6 col-xl-3" key={item.id}>
           <div className={`ff_one ${item.blockStyle}`}>
             <div className="detais">
